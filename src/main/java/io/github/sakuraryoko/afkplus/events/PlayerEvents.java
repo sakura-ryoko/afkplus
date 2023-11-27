fc0d2593e72f11db910bf6a1d49b10a6bd16fad7
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2023-11-27 11:46:14 -0500

    1.3.5 update

---
+++ src/main/java/io/github/sakuraryoko/afkplus/events/PlayerEvents.java
@@ -0,0 +1,5 @@
+package io.github.sakuraryoko.afkplus.events;
+
+public class PlayerEvents {
+    // Does nothing yet. For streamlining Player activity
+}
--- src/main/java/io/github/sakuraryoko/afkplus/placeholders/AfkPlusName.java
+++ src/main/java/io/github/sakuraryoko/afkplus/placeholders/AfkPlusName.java
@@ -26,5 +26,19 @@ public class AfkPlusName {
                             ctx);
             return PlaceholderResult.value(result);
         });
+        Placeholders.register(new Identifier(AFK_MOD_ID, "display_name"), (ctx, arg) -> {
+            if (!ctx.hasPlayer()) {
+                return PlaceholderResult.invalid("No player!");
+            }
+            AfkPlayerData player = (AfkPlayerData) ctx.player();
+            assert player != null;
+            Text result = player.isAfk()
+                    ? Placeholders.parseText(
+                            TextParserUtils.formatTextSafe(CONFIG.PlaceholderOptions.afkPlusNamePlaceholderAfk), ctx)
+                    : Placeholders.parseText(
+                            TextParserUtils.formatTextSafe(CONFIG.PlaceholderOptions.afkPlusNamePlaceholder),
+                            ctx);
+            return PlaceholderResult.value(result);
+        });
     };
 }
