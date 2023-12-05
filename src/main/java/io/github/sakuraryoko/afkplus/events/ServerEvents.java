00694cd2cd1532610c585f596ce0390315a69d4f
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2023-12-05 18:42:19 -0500

    removed IntegratedServerMixin for 1.20.3
    ... it has been removed from 1.20.3, but existed in 1.20.3-rc1.  Weird?

--- src/main/java/io/github/sakuraryoko/afkplus/events/ServerEvents.java
+++ src/main/java/io/github/sakuraryoko/afkplus/events/ServerEvents.java
@@ -5,23 +5,12 @@ import java.util.Collection;
 import io.github.sakuraryoko.afkplus.util.AfkPlusConflicts;
 import io.github.sakuraryoko.afkplus.util.AfkPlusLogger;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.integrated.IntegratedServer;
 
 public class ServerEvents {
     static private Collection<String> dpCollection;
 
     public static void starting(MinecraftServer server) {
         AfkPlusLogger.debug("Server is starting. " + server.getName());
-    }
-
-    public static void integratedServerStart(IntegratedServer server) {
-        if (server != null) {
-            dpCollection = server.getDataPackManager().getEnabledNames();
-            AfkPlusConflicts.checkDatapacks(dpCollection);
-            AfkPlusLogger.debug("Integrated Server has started. " + server.getName());
-        } else {
-            AfkPlusLogger.debug("Integrated Server has started. ");
-        }
     }
 
     public static void started(MinecraftServer server) {
@@ -38,14 +27,6 @@ public class ServerEvents {
 
     public static void stopping(MinecraftServer server) {
         AfkPlusLogger.debug("Server is stopping. " + server.getName());
-    }
-
-    public static void integratedServerStopping(IntegratedServer server) {
-        if (server != null) {
-            AfkPlusLogger.debug("Integrated Server is stopping. " + server.getName());
-        } else {
-            AfkPlusLogger.debug("Integrated Server is stopping. ");
-        }
     }
 
     public static void stopped(MinecraftServer server) {
