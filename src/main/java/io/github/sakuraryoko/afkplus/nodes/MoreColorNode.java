d186588057420accc2f6af9bd967da0f3cceb4ea
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2024-01-07 04:03:23 -0500

    1.4.3 - Adds Spectator Mode checks, adds flexible color nodes for use within AFKPlus only., adds /afkex command to help people learn to use the PlaceholderAPI nodes.

--- src/main/java/io/github/sakuraryoko/afkplus/nodes/MoreColorNode.java
+++ src/main/java/io/github/sakuraryoko/afkplus/nodes/MoreColorNode.java
@@ -1,7 +1,5 @@
 package io.github.sakuraryoko.afkplus.nodes;
 
-import com.mojang.serialization.DataResult;
-import io.github.sakuraryoko.afkplus.util.AfkPlusLogger;
 import net.minecraft.text.TextColor;
 import org.jetbrains.annotations.Nullable;
 
@@ -12,34 +10,40 @@ public class MoreColorNode {
     private final String name;
     private final String hexCode;
     private List<String> aliases = new ArrayList<>();
-    private TextColor color;
+    private final TextColor color;
 
     protected MoreColorNode(String name, String hexCode) {
-        DataResult<TextColor> dr;
-        dr = TextColor.parse(hexCode);
-        if (dr.error().isEmpty()) {
-            this.name = name;
-            this.hexCode = hexCode;
-            this.color = dr.get().left().orElse(null);
-        } else {
+        // 1.20.4
+        //DataResult<TextColor> dr;
+        //dr = TextColor.parse(hexCode);
+        //if (dr.error().isEmpty()) {
+        this.name = name;
+        this.hexCode = hexCode;
+        this.color = TextColor.parse(hexCode);
+        // 1.20.4
+/*        } else {
             AfkPlusLogger.warn("MoreColor("+ name +") is Invalid, error: "+dr.error().toString());
             this.name = "";
             this.hexCode = "";
         }
+        */
     }
     protected MoreColorNode(String name, String hexCode, @Nullable List<String> aliases) {
-        DataResult<TextColor> dr;
-        dr = TextColor.parse(hexCode);
-        if (dr.error().isEmpty()) {
-            this.name = name;
-            this.hexCode = hexCode;
-            this.color = dr.get().left().orElse(null);
-            this.aliases = aliases;
-        } else {
+        // 1.20.4
+        //DataResult<TextColor> dr;
+        //dr = TextColor.parse(hexCode);
+        //if (dr.error().isEmpty()) {
+        this.name = name;
+        this.hexCode = hexCode;
+        this.color = TextColor.parse(hexCode);
+        this.aliases = aliases;
+        // 1.20.4
+        /*} else {
             AfkPlusLogger.warn("MoreColor("+ name +") is Invalid, error: "+dr.error().toString());
             this.name = "";
             this.hexCode = "";
         }
+         */
     }
 
     protected String getName() { return this.name; }
